542c6ad4baa79a0925de864879ed805950781dec
Aman-Dhasmana <dhasmanaamann@gmail.com>
2025-05-29 15:27:39 +0530

    Added Exception Handling for the APIs and also used appropriate status code.

--- src/main/java/com/myProjects/quizApp/Controller/QuestionControler.java
+++ src/main/java/com/myProjects/quizApp/Controller/QuestionControler.java
@@ -3,6 +3,7 @@ package com.myProjects.quizApp.Controller;
 import com.myProjects.quizApp.Model.Question;
 import com.myProjects.quizApp.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +16,23 @@ public class QuestionControler {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestion();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return  questionService.addQuestion(question);
     }
 
+
     @DeleteMapping("remove/{id}")
-    public String removeQuestionById(@PathVariable Integer id){
+    public ResponseEntity<String> removeQuestionById(@PathVariable Integer id){
         return questionService.removeQuestionById(id);
     }
 
