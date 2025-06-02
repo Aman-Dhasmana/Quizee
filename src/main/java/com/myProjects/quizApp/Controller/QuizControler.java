200e66a17a0fceb45996ac9a20aa2beb34aede8e
Aman-Dhasmana <dhasmanaamann@gmail.com>
2025-06-02 19:07:37 +0530

    Added Functionality to play quiz and also get the results.

--- src/main/java/com/myProjects/quizApp/Controller/QuizControler.java
+++ src/main/java/com/myProjects/quizApp/Controller/QuizControler.java
@@ -1,13 +1,15 @@
 package com.myProjects.quizApp.Controller;
 
+import com.myProjects.quizApp.Model.Question;
+import com.myProjects.quizApp.Model.QuestionWrapper;
+import com.myProjects.quizApp.Model.Response;
 import com.myProjects.quizApp.Service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -18,5 +20,15 @@ public class QuizControler {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int noQ, @RequestParam String title){
         return quizService.createQuiz(category,noQ,title);
+    }
+
+    @GetMapping ("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestion(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id,responses);
     }
 }
--- src/main/java/com/myProjects/quizApp/Service/QuizService.java
+++ src/main/java/com/myProjects/quizApp/Service/QuizService.java
@@ -3,13 +3,17 @@ package com.myProjects.quizApp.Service;
 import com.myProjects.quizApp.Dao.QuestionDao;
 import com.myProjects.quizApp.Dao.QuizDao;
 import com.myProjects.quizApp.Model.Question;
+import com.myProjects.quizApp.Model.QuestionWrapper;
 import com.myProjects.quizApp.Model.Quiz;
+import com.myProjects.quizApp.Model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -30,5 +34,33 @@ public class QuizService {
         quizDao.save(quiz);
 
        return new ResponseEntity<>("Success !!", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(Integer id) {
+         Optional<Quiz> quiz= quizDao.findById(id);
+         List<Question> questionsFroomDB = quiz.get().getQuestions();
+         List<QuestionWrapper> questionForUser = new ArrayList<>();
+
+         for(Question q : questionsFroomDB){
+             QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+             questionForUser.add(qw);
+         }
+
+          return new ResponseEntity<>(questionForUser,HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int result = 0;
+        int i=0;
+
+        for (Response response : responses){
+            if (response.getResponse().equals(questions.get(i).getCorrectOption()))
+                result++;
+
+            i++;
+        }
+        return new ResponseEntity<>(result,HttpStatus.OK);
     }
 }
